ad3e1ac646bd30602ecf9fbdbe2d30cfe74aadc6
poppyharandtika <56400553+poppyharandtika@users.noreply.github.com>
2019-10-25 22:05:34 -0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,15 @@
+namalengkap = "Poppy Harandtika"
+namapanggilan = "Poppy"
+npm = "311910687"
+tempatlahir = "Bekasi"
+umur = "18"
+telp = "0896-2321-8728"
+alamat = "Tambun Selatan"
+
+print("ssalamualaikum,\n"
+      "let me introduce my self, my name is",namalengkap,
+      "but you can call me",namapanggilan,
+      ", my npm is",npm,
+      ", i was born in",tempatlahir,
+      "and i am",umur,"years old. i am very glad if you want to invite my house",alamat,",so don't forget to call me before with the number",telp,"\n thank you")
+
